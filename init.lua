f53de67d026174ccf1856b280604409379297724
Masaya Takeda <844196@gmail.com>
2018-12-05 04:20:58 +0900

    右オプションでカレントウィンドウを最大化

--- init.lua
+++ init.lua
@@ -1,12 +1,27 @@
+local handleKeys = {
+  rcmd = {keyCode = 54, keyDownFlag = 1048848},
+  roption = {keyCode = 61, keyDownFlag = 524608},
+  rshift = {keyCode = 60, keyDownFlag = 131332},
+}
+
 eventtap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function (event)
-  if event:getKeyCode() ~= 60 then
+  if event:getKeyCode() ~= handleKeys.roption.keyCode then
     return false
   end
-  if event:getRawEventData().NSEventData.modifierFlags ~= 131332 then
+  if event:getRawEventData().NSEventData.modifierFlags ~= handleKeys.roption.keyDownFlag then
     return false
   end
 
-  hs.logger.new('my', 'debug').i('keydown rshift')
+  local win = hs.window.focusedWindow()
+  local tileUnit = '[0.5, 1, 99.5, 99]'
+
+  if (win:frame() == win:screen():fromUnitRect(tileUnit)) then
+    hs.grid.pushWindowNextScreen()
+    win:moveToUnit(tileUnit)
+  else
+    win:moveToUnit(tileUnit)
+  end
+
   return true
 end)
 eventtap:start()
