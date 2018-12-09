8779a5f0363f6bad3bfc29432684d3ece46eea2f
Masaya Takeda <844196@gmail.com>
2018-12-09 15:13:40 +0900

    右シフトで縦分割

--- init.lua
+++ init.lua
@@ -1,27 +1,77 @@
+local function min (table)
+  local key = next(table)
+  local min = table[key]
+  for k, v in pairs(table) do
+    if (v < min) then
+      key, min = k, v
+    end
+  end
+  return min, key
+end
+
+local function eventtapModKeySinglePress (key, callback)
+  return hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function (event)
+    if event:getKeyCode() ~= key.keyCode then
+      return false
+    end
+    if event:getRawEventData().NSEventData.modifierFlags ~= 256 then
+      return false
+    end
+    return callback()
+  end)
+end
+
+local function computeNextTile (window, tiles)
+  local tilesAsGeometry = hs.fnutils.map(tiles, function (tile)
+    return window:screen():fromUnitRect(tile)
+  end)
+  local currentTileId = hs.fnutils.indexOf(tilesAsGeometry, window:frame())
+
+  if (not currentTileId) then
+    local points = hs.fnutils.map(tilesAsGeometry, function (geom)
+      return window:frame():distance(geom.topleft)
+    end)
+    local _, nearestTileId = min(points)
+    return tiles[nearestTileId], false
+  end
+
+  local nextTile = tiles[currentTileId + 1]
+  if (not nextTile) then
+    return tiles[1], true
+  end
+
+  return nextTile, false
+end
+
+local function applyTilingRule (tiles)
+  return function ()
+    local win = hs.window.focusedWindow()
+
+    local nextTile, shouldMoveNextScreen = computeNextTile(win, tiles)
+
+    if (shouldMoveNextScreen) then
+      hs.grid.pushWindowNextScreen()
+    end
+    win:moveToUnit(nextTile)
+  end
+end
+
 local handleKeys = {
   rcmd = {keyCode = 54, keyDownFlag = 1048848},
   roption = {keyCode = 61, keyDownFlag = 524608},
   rshift = {keyCode = 60, keyDownFlag = 131332},
 }
+local rules = {
+  only = {
+    '[0.5, 1, 99.5, 99]',
+  },
+  vs = {
+    '[0.5, 1, 49.75, 99]',
+    '[50.25, 1, 99.5, 99]',
+  },
+}
 
-eventtap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function (event)
-  if event:getKeyCode() ~= handleKeys.roption.keyCode then
-    return false
-  end
-  if event:getRawEventData().NSEventData.modifierFlags ~= handleKeys.roption.keyDownFlag then
-    return false
-  end
-
-  local win = hs.window.focusedWindow()
-  local tileUnit = '[0.5, 1, 99.5, 99]'
-
-  if (win:frame() == win:screen():fromUnitRect(tileUnit)) then
-    hs.grid.pushWindowNextScreen()
-    win:moveToUnit(tileUnit)
-  else
-    win:moveToUnit(tileUnit)
-  end
-
-  return true
-end)
-eventtap:start()
+roptionSingle = eventtapModKeySinglePress(handleKeys.roption, applyTilingRule(rules.only))
+roptionSingle:start()
+rshiftSingle = eventtapModKeySinglePress(handleKeys.rshift, applyTilingRule(rules.vs))
+rshiftSingle:start()
